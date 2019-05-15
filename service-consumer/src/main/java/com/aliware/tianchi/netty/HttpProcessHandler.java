c86021479f09057ec26e5983ca9564ec4efa1658
项升 <xiangsheng.gh@alibaba-inc.com>
2019-05-15 13:54:26 +0800

    Update url config

--- service-consumer/src/main/java/com/aliware/tianchi/netty/HttpProcessHandler.java
+++ service-consumer/src/main/java/com/aliware/tianchi/netty/HttpProcessHandler.java
@@ -91,10 +91,10 @@ public class HttpProcessHandler extends SimpleChannelInboundHandler<FullHttpRequ
 
     private List<URL> buildUrls(String interfaceName, Map<String, String> attributes) {
         List<URL> urls = new ArrayList<>();
+        // 配置直连的 provider 列表
         urls.add(new URL(Constants.DUBBO_PROTOCOL, "provider-small", 20880, interfaceName, attributes));
-//        urls.add(new URL(Constants.DUBBO_PROTOCOL, "provider-medium", 20870, interfaceName, attributes));
-//        urls.add(new URL(Constants.DUBBO_PROTOCOL, "provider-large", 20890, interfaceName, attributes));
-    urls.add(new URL(Constants.DUBBO_PROTOCOL, "localhost", 20880, interfaceName, attributes));
+        urls.add(new URL(Constants.DUBBO_PROTOCOL, "provider-medium", 20870, interfaceName, attributes));
+        urls.add(new URL(Constants.DUBBO_PROTOCOL, "provider-large", 20890, interfaceName, attributes));
         return urls;
     }
 
